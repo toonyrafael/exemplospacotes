16282731b976d2f1928e154502ecc0b26b7a0712
toonyrafael <toonyrafael@gmail.com>
2025-07-03 11:47:55 -0300

    ENCAPSULAMENTO FEITO

--- src/messenger/MSNMessenger.java
+++ src/messenger/MSNMessenger.java
@@ -2,15 +2,17 @@ package messenger;
 
 public class MSNMessenger {
 	public void enviarMensagem() {
+		validarConectadoInternet();
 		System.out.println("Enviando mensagem");
+		salvarHistoricoMensagem();
 	}
 	public void receberMensagem() {
 		System.out.println("Recebendo mensagem");
 	}
-	public void validarConectadoInternet() {
+	private void validarConectadoInternet() {
 		System.out.println("Validando se está conectado a internet");
 	}
-	public void salvarHistoricoMensagem() {
+	private void salvarHistoricoMensagem() {
 		System.out.println("Salvando o histórico da mensagem");
 	}
 }
